8444eac44d43c68e82b8adea17231b1d142d57a4
caleb1464 <muriukicaleb20@gmail.com>
2024-03-08 11:29:36 +0300

    initialcommit

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
     path('delete/<item_id>', views.delete, name='delete'),
     path('crossoff/<item_id>', views.cross_off, name='cross_off'),
     path('uncross/<item_id>', views.uncross, name='uncross'),
